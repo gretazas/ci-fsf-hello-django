3974edc2f09e173abda207f82b948c21b451a8df
gretazas <esminis@yahoo.com>
2022-08-09 12:19:00 +0000

    Add edit_item

--- todo/views.py
+++ todo/views.py
@@ -13,13 +13,30 @@ def get_todo_list(request):
 
 def add_item(request):
     if request.method == "POST":
-        name = request.POSTget('item_name')
-        done = done in request.POST
-        Item.Objects.create(name=name, done=done)
-        return redirect('get_todo_list')
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.seve()
+            # name = request.POSTget('item_name')
+            # done = done in request.POST
+            # Item.Objects.create(name=name, done=done)
+            return redirect('get_todo_list')
 
     form = ItemForm()
     context = {
         'form': form
     }
     return render(request, 'todo/add_item.html', context)
+
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method =='POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.seve()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/etit_item.html', context)
