15d4af18551a43208b267ae84661f073817f03ff
gretazas <esminis@yahoo.com>
2022-08-12 08:07:18 +0000

    Set SECRET_KEY in workspace instead

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '*c1x-_e9g*_4h0=k=m(-vd%#q2!^fkvvxa@e(sw(er2@2(j@u&')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
